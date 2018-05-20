bd50f496a362bc49f994e8bf5c00fba6d675ffdf
mrassett <magsrassett@gmail.com>
2018-05-20 17:40:03 -0500

    completed

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -1,6 +1,7 @@
 let mysql = require("mysql");
 let inquirer = require("inquirer");
-let selected = [];
+require("console.table");
+
 
 // create the connection information for the sql database
 let connection = mysql.createConnection({
@@ -16,8 +17,11 @@ connection.connect(function(error) {
     console.log("connected as id " + connection.threadId);
     queryProducts()
   });
+
+
   function queryProducts() {
     connection.query("SELECT * FROM products", function(error, response) {
+      if (error) console.log(error);
       for (var i = 0; i < response.length; i++) {
         console.log(response[i].product_name + " | " + response[i].department_name + " | " + response[i].price + " | " + response[i].stock_quantity);
       }
@@ -31,38 +35,57 @@ inquirer
       name: "Purchase",
       type: "input",
       message: "What would you like to purchase?"
-    },
-    {
-      name: "quantity",
-      type: "input",
-      message: "How many would you like?"
-      },
-
+    }
   ])
   .then(function(inquirerResponse) {
-  //if else statement or switch case goes here
-  var quantity = parseInt(inquirerResponse.quantity)
-  let chosenProduct = parseInt(inquirerResponse.choice);
-  console.log(chosenProduct);
-  let product = updateInventory(chosenProduct, inventory);
+  let chosenProduct = inquirerResponse.choice;
+  let product = checkHowMany(chosenProduct, inventory);
+  
   if (product){
-    promptHowMany(product)
-  } else  if (quantity > product.stock_quantity) {
-    console.log("Sorry, we don't have enough of that product in stock");
-    selectedProduct();
+    promptHowMany(product) 
   } else {
-    updateInventory(product, quantity);
+    console.log("Sorry, we don't have that product in stock");
     selectedProduct();
   }
   });
 }
+
+function promptHowMany(product){
+inquirer
+  .prompt([{
+        name: "quantity",
+        type: "input",
+        message: "How many would you like?"
+
+  }
+ ]).then(function(inquirerResponse){
+  let quantity = parseInt(inquirerResponse.quantity);
+  if (quantity > product.stock_quantity){
+    console.log("\n Stock Not Available")
+    queryProducts()
+  } else {
+    updateInventory()
+  }
+
+
+})
 function updateInventory(product, quantity){
 connection.query(
-  "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?", 
-  [quantity, product.item_id], 
+  "UPDATE products SET stock_quantity = stock_quantity - 1", 
   function(err,res){
-    console.log("\n Thank you for your purchase of" + quantity + product.product_name);
+    if (err) throw err;
+    console.log("\n Thank you for your purchase");
     selectedProduct();
   }
 )
 }
+}
+function checkHowMany(chosenProduct, inventory) {
+  for (var i = 0; i < inventory.length; i++) {
+    if (inventory.product_name === chosenProduct) {
+      return inventory;
+    }
+  }
+  return null;
+}
+
